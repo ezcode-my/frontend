824cb31d4049a254d3239367e13baa722c5af166
leetaewon <tw@stayorder.co.kr>
2025-07-02 19:23:03 +0900

    feat : 문제 리스트 퍼블중

--- src/api/interceptor/response.interceptor.ts
+++ src/api/interceptor/response.interceptor.ts
@@ -1,36 +1,34 @@
-
-import { IApiResponse } from "./interceptor.interface";
-import { toast } from "sonner";
+import { IApiResponse } from './interceptor.interface';
+import { toast } from 'sonner';
 
 /**
  * 응답 인터셉터
  * 모든 API 응답을 처리하는 함수
  */
 
-const ERROR_THROW_PATHS = [
-    '/auth/signin',
-];
+const ERROR_THROW_PATHS = ['/auth/signin'];
 export const responseInterceptor = async <T>(response: Response): Promise<IApiResponse<T>> => {
-    /** 응답 데이터 JSON 파싱 */
-    const data = await response.json();
-    if (!response.ok) {
-        const isAuthPath = ERROR_THROW_PATHS.some(path => response.url.includes(path));
-        if (response.status === 400) {
-            if (!isAuthPath) {
-                toast(data.message);
-            }
-        }
-        if (isAuthPath) {
-            throw {
-                status: response.status,
-                message: data.message,
-                code: data.code
-            };
-        } else {
-            throw new Error(data.message);
-        }
+  /** 응답 데이터 JSON 파싱 */
+  const data = await response.json();
+  if (!response.ok) {
+    const isAuthPath = ERROR_THROW_PATHS.some((path) => response.url.includes(path));
+    if (response.status === 400) {
+      // if (!isAuthPath) {
+      //     toast(data.message);
+      // }
     }
-    return {
-        data,
-    };
+    if (isAuthPath) {
+      throw {
+        status: response.status,
+        message: data.message,
+        code: data.code,
+      };
+    }
+    // else {
+    //   throw new Error(data.message);
+    // }
+  }
+  return {
+    data,
+  };
 };
