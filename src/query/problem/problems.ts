48c6dadfe96a10a0f4715cf740d9f8613efd3ed2
leetaewon <tw@stayorder.co.kr>
2025-07-03 18:32:20 +0900

    feat : problem-list api 연동

---
+++ src/query/problem/problems.ts
@@ -0,0 +1,17 @@
+import ApiHelper from '@/api/client/api';
+import { useQuery } from '@tanstack/react-query';
+import { ProblemList } from './problem.interface';
+
+export const useProblemListQuery = (page: string, size: string, sort: string) => {
+  return useQuery({
+    queryKey: ['problemList', page, size, sort], // 쿼리 키는 파라미터별로 다르게
+    queryFn: async () => {
+      const response = await ApiHelper.get<ProblemList>('/problems', {
+        params: { page, size, sort },
+      });
+      return response;
+    },
+
+    staleTime: 1000 * 60 * 5, // 5분간 캐시 유지
+  });
+};
