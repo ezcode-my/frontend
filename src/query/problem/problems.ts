8f242f501ff0c404e4ef8a37b718e29fd003ddf7
leetaewon <tw@stayorder.co.kr>
2025-07-04 18:00:07 +0900

    fix : select, button 컴포넌트로 변경, 페이지네이션 기능 수정

--- src/query/problem/problems.ts
+++ src/query/problem/problems.ts
@@ -2,13 +2,13 @@ import ApiHelper from '@/api/client/api';
 import { useQuery } from '@tanstack/react-query';
 import { ProblemList } from './problem.interface';
 
-export const useProblemListQuery = (page: string, size: string, sort: string) => {
+export const useProblemListQuery = (page: number, size: number, sort: string) => {
   return useQuery({
     queryKey: ['problemList', page, size, sort], // 쿼리 키는 파라미터별로 다르게
     queryFn: async () => {
-      const response = await ApiHelper.get<ProblemList>('/problems', {
-        params: { page, size, sort },
-      });
+      const response = await ApiHelper.get<ProblemList>(
+        `/problems?page=${page}&size=${size}sort=${sort}`
+      );
       return response;
     },
 
