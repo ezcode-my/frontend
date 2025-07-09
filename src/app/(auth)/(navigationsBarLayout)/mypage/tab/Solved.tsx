7d0c990dbf87e5321ad2b3355a3ed2af7c537a24
leetaewon <tw@stayorder.co.kr>
2025-07-09 16:17:19 +0900

    feat : mypage 푼 문제 목록 탭 작업중

--- src/app/(auth)/(navigationsBarLayout)/mypage/tab/Solved.tsx
+++ src/app/(auth)/(navigationsBarLayout)/mypage/tab/Solved.tsx
@@ -2,43 +2,23 @@ import { useSubmissionList } from '@/query/mypage/mypage';
 import { useModalStore } from '@/shared/modal/model/modalStore';
 
 export const Solved = () => {
-  const { data } = useSubmissionList();
-  console.log(data?.result);
-  console.log(data?.result.length);
-  const { open } = useModalStore();
   return (
-    <div className="w-full flex flex-col">
+    <div>
       <table>
         <thead>
           <tr className="border-b border-gray-700">
-            <th className="py-3 px-2 text-center">문제 번호</th>
             <th className="py-3 px-2 text-center">문제 이름</th>
-            <th className="py-3 px-2 text-center">정답 제출 수 </th>
-            <th className="py-3 px-2 text-center">총 제출 수 </th>
+            <th className="py-3 px-2 text-center">푼 날짜</th>
+            <th className="py-3 px-2 text-center">성공 여부</th>
           </tr>
         </thead>
-        {(data?.result.length || 0) > 0 && (
-          <tbody>
-            {data?.result.map((item, index) => {
-              return (
-                <tr
-                  onClick={() => {
-                    open('solved', data.result[index]);
-                  }}
-                  key={item.problemId}
-                  className="border-b border-gray-800 hover:bg-gray-900 cursor-pointer"
-                >
-                  <td className="text-center py-3 px-2">{item.problemId}</td>
-                  <td className="text-center py-3 px-2">{item.problemTitle}</td>
-                  <td className="text-center py-3 px-2">
-                    {item.submissions.filter((submission) => submission.isCorrect).length}
-                  </td>
-                  <td className="text-center py-3 px-2">{item.submissions.length}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        )}
+        <tbody>
+          <tr className="border-b border-gray-800 hover:bg-gray-900 cursor-pointer">
+            <td className="text-center py-3 px-2">1</td>
+            <td className="text-center py-3 px-2">{1}</td>
+            <td className="text-center py-3 px-2">1</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
