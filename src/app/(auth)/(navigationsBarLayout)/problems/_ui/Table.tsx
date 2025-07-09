cb631ef6568257898c31320cb70d93d0fc2fe8f1
leetaewon <tw@stayorder.co.kr>
2025-07-09 14:06:01 +0900

    feat : 검색어 필터 기능 작업 중

--- src/app/(auth)/(navigationsBarLayout)/problems/_ui/Table.tsx
+++ src/app/(auth)/(navigationsBarLayout)/problems/_ui/Table.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useState, useEffect, useMemo } from 'react';
-
-import { SkeletonBox } from '@/components/Skeleton';
-import { useProblemListQuery } from '@/query/problem/problems';
+import React, { useMemo, useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { SkeletonBox } from '@/components/Skeleton';
+import { ProblemList } from '@/query/problem/problem.interface';
 
 const PAGE_LIMIT = 15;
 
@@ -18,23 +17,26 @@ const getLevelColorClass = (levelStr: string): string => {
 };
 
 export default function ProblemTable({
-  difficulty,
-  categoryCode,
+  data,
+  isLoading,
+  currentPage,
+  setCurrentPage,
+  totalPages,
 }: {
-  difficulty: string;
-  categoryCode: string;
+  data: ProblemList | undefined;
+  isLoading: boolean;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+  totalPages: number;
 }) {
   const router = useRouter();
-  const [currentPage, setCurrentPage] = useState(1);
   const [pageGroupStart, setPageGroupStart] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const { data, isLoading } = useProblemListQuery(currentPage, 10, '', categoryCode, difficulty);
 
   useEffect(() => {
-    if (data?.data.result.totalPages) {
-      setTotalPages(data.data.result.totalPages - 1);
+    if (currentPage < pageGroupStart || currentPage >= pageGroupStart + PAGE_LIMIT) {
+      setPageGroupStart(Math.floor((currentPage - 1) / PAGE_LIMIT) * PAGE_LIMIT + 1);
     }
-  }, [data]);
+  }, [currentPage]);
 
   const pageNumbers = useMemo(() => {
     const end = Math.min(pageGroupStart + PAGE_LIMIT - 1, totalPages);
@@ -52,10 +54,11 @@ export default function ProblemTable({
     setPageGroupStart((prev) => prev + PAGE_LIMIT);
     setCurrentPage(pageGroupStart + PAGE_LIMIT);
   };
-
+  useEffect(() => {
+    console.log(isLoading, data);
+  }, [isLoading, data]);
   return (
     <div className="flex flex-col w-full text-white font-sans bg-black rounded-md p-4">
-      {/* 테이블 */}
       <table className="w-full border-collapse text-sm">
         <thead>
           <tr className="border-b border-gray-700">
@@ -69,7 +72,7 @@ export default function ProblemTable({
           </tr>
         </thead>
         <tbody>
-          {isLoading
+          {isLoading || !data?.content
             ? Array.from({ length: 10 }).map((_, idx) => (
                 <tr key={idx} className="border-b border-gray-800">
                   {Array.from({ length: 7 }).map((__, colIdx) => (
@@ -79,13 +82,11 @@ export default function ProblemTable({
                   ))}
                 </tr>
               ))
-            : data?.data.result.content.map((item) => (
+            : data.content.map((item: any) => (
                 <tr
                   key={item.id}
                   className="border-b border-gray-800 hover:bg-gray-900 cursor-pointer"
-                  onClick={() => {
-                    router.push(`/problems/${item.id}`);
-                  }}
+                  onClick={() => router.push(`/problems/${item.id}`)}
                 >
                   <td className="text-center py-3 px-2">{item.id}</td>
                   <td className="text-center py-3 px-2">{item.title}</td>
@@ -115,7 +116,6 @@ export default function ProblemTable({
           width={8}
           height={8}
           alt="page-arrow"
-          priority
           onClick={handlePrevGroup}
         />
 
@@ -137,7 +137,6 @@ export default function ProblemTable({
           width={8}
           height={8}
           alt="page-arrow"
-          priority
           onClick={handleNextGroup}
         />
       </div>
--- src/app/(auth)/(navigationsBarLayout)/problems/page.tsx
+++ src/app/(auth)/(navigationsBarLayout)/problems/page.tsx
@@ -9,71 +9,91 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import ProblemTable from './_ui/Table';
+import { useProblemListQuery } from '@/query/problem/problems';
 
 const ProblemsList = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const categoryCodeItem = ['전체', 'BFS', 'DFS', '수학', '조건문'];
   const difficultyItem = ['전체', 'LV1', 'LV2', 'LV3', 'LV4', 'LV5', 'LV6', 'LV7'];
   const [categoryCode, setCategoryCode] = useState('');
   const [difficulty, setDifficulty] = useState('');
+  const [keyword, setKeyword] = useState('');
+  const [search, setSearch] = useState('');
 
+  const { data, isLoading } = useProblemListQuery(
+    currentPage,
+    10,
+    '',
+    categoryCode,
+    difficulty,
+    search
+  );
+
+  const totalPages = data?.data.result.totalPages ?? 0;
+  useEffect(() => {
+    console.log('data', data?.data.result);
+  }, [data]);
   return (
     <div className="flex flex-col px-10 py-18 w-full pt-[140px] gap-4">
       <section className="flex flex-col gap-10">
-        <div className="flex flex-row justify-between items-center  ">
+        <div className="flex flex-row justify-between items-center">
           <div className="flex gap-3">
-            <Select
-              onValueChange={(value) => {
-                setCategoryCode(value);
-              }}
-            >
+            <Select onValueChange={setCategoryCode}>
               <SelectTrigger>
-                <SelectValue placeholder="카테고리"></SelectValue>
+                <SelectValue placeholder="카테고리" />
               </SelectTrigger>
-              <SelectContent style={{ zIndex: 999, position: 'relative', backgroundColor: '#000' }}>
+              <SelectContent>
                 <SelectGroup>
-                  {categoryCodeItem.map((item) => {
-                    return (
-                      <SelectItem key={item} value={item}>
-                        {item}
-                      </SelectItem>
-                    );
-                  })}
+                  {categoryCodeItem.map((item) => (
+                    <SelectItem key={item} value={item}>
+                      {item}
+                    </SelectItem>
+                  ))}
                 </SelectGroup>
               </SelectContent>
             </Select>
 
-            <Select
-              onValueChange={(value) => {
-                setDifficulty(value);
-              }}
-            >
+            <Select onValueChange={setDifficulty}>
               <SelectTrigger>
-                <SelectValue placeholder="난이도"></SelectValue>
+                <SelectValue placeholder="난이도" />
               </SelectTrigger>
-              <SelectContent style={{ zIndex: 999, position: 'relative', backgroundColor: '#000' }}>
+              <SelectContent>
                 <SelectGroup>
-                  {difficultyItem.map((item) => {
-                    return (
-                      <SelectItem key={item} value={item}>
-                        {item}
-                      </SelectItem>
-                    );
-                  })}
+                  {difficultyItem.map((item) => (
+                    <SelectItem key={item} value={item}>
+                      {item}
+                    </SelectItem>
+                  ))}
                 </SelectGroup>
               </SelectContent>
             </Select>
           </div>
+
           <div className="flex flex-row gap-1 bg-[#30333C] rounded-[25px] px-5 py-3">
-            <input className="min-w-[400px] border-none outline-none" />
+            <input
+              className="min-w-[400px] border-none outline-none bg-transparent text-white"
+              onChange={(e) => setKeyword(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  setSearch(keyword);
+                  setCurrentPage(1);
+                }
+              }}
+            />
             <svg
               width="36"
               height="37"
               viewBox="0 0 36 37"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              onClick={() => {
+                setSearch(keyword);
+                setCurrentPage(1);
+              }}
+              className="cursor-pointer"
             >
               <path
                 d="M15.7751 0.734863C24.1269 0.734863 30.898 7.50521 30.8982 15.8569L30.8933 16.2476C30.809 19.5745 29.65 22.6344 27.7507 25.0933L35.4509 32.7935L31.9158 36.3286L24.0818 28.4946C21.6975 30.0649 18.8434 30.98 15.7751 30.98L15.3855 30.9751C7.21385 30.7682 0.653076 24.0784 0.653076 15.8569C0.653281 7.50534 7.42355 0.735068 15.7751 0.734863ZM15.7751 5.73486C10.185 5.73507 5.65328 10.2668 5.65308 15.8569C5.65308 21.4473 10.1848 25.9798 15.7751 25.98C21.3656 25.98 25.8982 21.4474 25.8982 15.8569C25.898 10.2666 21.3655 5.73486 15.7751 5.73486Z"
@@ -82,6 +102,7 @@ const ProblemsList = () => {
             </svg>
           </div>
         </div>
+
         {(categoryCode !== '전체' || difficulty !== '전체') && (
           <div className="flex flex-row gap-8">
             {categoryCode !== '전체' && categoryCode && (
@@ -93,9 +114,7 @@ const ProblemsList = () => {
                   alt="close"
                   width={16}
                   height={16}
-                  onClick={() => {
-                    setCategoryCode('');
-                  }}
+                  onClick={() => setCategoryCode('')}
                 />
               </div>
             )}
@@ -108,16 +127,21 @@ const ProblemsList = () => {
                   alt="close"
                   width={16}
                   height={16}
-                  onClick={() => {
-                    setDifficulty('');
-                  }}
+                  onClick={() => setDifficulty('')}
                 />
               </div>
             )}
           </div>
         )}
       </section>
-      <ProblemTable categoryCode={categoryCode} difficulty={difficulty} />
+
+      <ProblemTable
+        data={data?.data.result}
+        isLoading={isLoading}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalPages={totalPages}
+      />
     </div>
   );
 };
