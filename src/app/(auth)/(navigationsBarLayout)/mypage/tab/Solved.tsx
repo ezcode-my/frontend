100ddaa92fd909cb20c7967f0c20f64db3082ea1
leetaewon <tw@stayorder.co.kr>
2025-07-10 19:02:07 +0900

    feat : 푼 문제 목록 작업 중

--- src/app/(auth)/(navigationsBarLayout)/mypage/tab/Solved.tsx
+++ src/app/(auth)/(navigationsBarLayout)/mypage/tab/Solved.tsx
@@ -1,24 +1,39 @@
 import { useSubmissionList } from '@/query/mypage/mypage';
-import { useModalStore } from '@/shared/modal/model/modalStore';
 
 export const Solved = () => {
+  const { data } = useSubmissionList();
+  console.log(data?.result);
+  console.log(data?.result.length);
   return (
     <div className="w-full flex flex-col">
       <table>
         <thead>
           <tr className="border-b border-gray-700">
+            <th className="py-3 px-2 text-center">문제 번호</th>
             <th className="py-3 px-2 text-center">문제 이름</th>
-            <th className="py-3 px-2 text-center">푼 날짜</th>
-            <th className="py-3 px-2 text-center">성공 여부</th>
+            <th className="py-3 px-2 text-center">정답 제출 수 </th>
+            <th className="py-3 px-2 text-center">총 제출 수 </th>
           </tr>
         </thead>
-        <tbody>
-          <tr className="border-b border-gray-800 hover:bg-gray-900 cursor-pointer">
-            <td className="text-center py-3 px-2">1</td>
-            <td className="text-center py-3 px-2">{1}</td>
-            <td className="text-center py-3 px-2">1</td>
-          </tr>
-        </tbody>
+        {(data?.result.length || 0) > 0 && (
+          <tbody>
+            {data?.result.map((item) => {
+              return (
+                <tr
+                  key={item.problemId}
+                  className="border-b border-gray-800 hover:bg-gray-900 cursor-pointer"
+                >
+                  <td className="text-center py-3 px-2">{item.problemId}</td>
+                  <td className="text-center py-3 px-2">{item.problemDescription}</td>
+                  <td className="text-center py-3 px-2">
+                    {item.submissions.filter((submission) => submission.isCorrect).length}
+                  </td>{' '}
+                  <td className="text-center py-3 px-2">{item.submissions.length}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        )}
       </table>
     </div>
   );
