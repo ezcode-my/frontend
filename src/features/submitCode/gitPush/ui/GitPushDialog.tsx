035221e8faf833425b057920b38b8a99897f1495
grimza99 <gbtmxlf@naver.com>
2025-07-30 19:15:49 +0900

    style: git 모달 스타일링

--- src/features/submitCode/gitPush/ui/GitPushDialog.tsx
+++ src/features/submitCode/gitPush/ui/GitPushDialog.tsx
@@ -2,9 +2,7 @@
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
-  DialogFooter,
   DialogHeader,
-  DialogClose,
   DialogContent,
   DialogDescription,
   DialogTitle,
@@ -13,65 +11,83 @@ import {
 import {
   Select,
   SelectContent,
-  SelectGroup,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
 import { Icon } from '@/shared';
 import useGitPush from '../hooks/useGitPush';
+import { Switch } from '@/components/ui/switch';
 
 interface GitPushDialogProps {
   githubUrl: string | null;
 }
 export default function GitPushDialog({ githubUrl }: GitPushDialogProps) {
   const { pushAutoToggle, choiceRepo, userRepos, currentRepo, setCurrentRepo } = useGitPush();
-
+  const autoCommit = true;
   return (
     <Dialog>
       <DialogTrigger>
         <Icon.TerminalGitHubIcon disabled={!!githubUrl} />
       </DialogTrigger>
-      <DialogContent className="w-[425px]">
+      <DialogContent className="w-[425px] bg-secondary-background border-[#333]">
         <DialogHeader>
-          <DialogTitle>git push</DialogTitle>
-          <DialogDescription>아래 선택하신 레포에 자동 push 됩니다</DialogDescription>
+          <DialogTitle>GitHub 연동</DialogTitle>
+          <DialogDescription className="text-sm font-medium text-[#ccc] mb-2 block">
+            아래 선택하신 repository에 자동 push 됩니다
+          </DialogDescription>
         </DialogHeader>
-        <Select>
-          <SelectTrigger>
-            <SelectValue placeholder={currentRepo}></SelectValue>
-          </SelectTrigger>
-          <SelectContent>
-            {userRepos ? (
-              <SelectGroup>
-                {userRepos.map((repo, idx) => (
-                  <SelectItem
-                    key={idx}
-                    value={repo.repoName}
-                    onClick={() => setCurrentRepo(repo.repoName)}
-                  >
-                    {repo.repoName} default branch : {repo.defaultBranch}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            ) : (
-              <div>레포 불러오기 실패</div>
-            )}
-          </SelectContent>
-        </Select>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="outline">닫기</Button>
-          </DialogClose>
-          <Button type="button" onClick={() => pushAutoToggle()}>
-            자동 push 토글버튼
-          </Button>
-          <DialogClose asChild>
-            <Button type="button" onClick={() => choiceRepo({ repositoryName: currentRepo })}>
-              레포선택
+
+        <div className="space-y-4">
+          <>
+            <label className="text-sm font-medium text-[#ccc] mb-2 block">레포지토리 선택</label>
+
+            <Select value={currentRepo}>
+              <SelectTrigger
+                className="bg-background border-[#333] text-white"
+                disabled={!userRepos}
+              >
+                <SelectValue
+                  placeholder={userRepos ? '레포지토리를 선택하세요' : '불러오기 실패'}
+                />
+              </SelectTrigger>
+              <SelectContent className="bg-secondary-background border-[#333]">
+                {userRepos ? (
+                  <ul className="overflow-y-auto max-h-60">
+                    {userRepos.map((repo, idx) => (
+                      <SelectItem
+                        key={idx}
+                        value={repo.repoName}
+                        onClick={() => setCurrentRepo(repo.repoName)}
+                        className="text-white hover:primary flex justify-between"
+                      >
+                        <p className="text-secondary text-md"> {repo.repoName}</p>
+                        <p className="text-xs text-[#ccc]">branch : {repo.defaultBranch}</p>
+                      </SelectItem>
+                    ))}
+                  </ul>
+                ) : null}
+              </SelectContent>
+            </Select>
+          </>
+          <div className="flex items-center justify-between">
+            <label className="text-sm font-medium text-[#ccc]">자동 푸시</label>
+            <Switch
+              checked={autoCommit}
+              onCheckedChange={() => pushAutoToggle()}
+              className="data-[state=checked]:bg-[#00d084]"
+            />
+          </div>
+          <div className="flex gap-2">
+            <Button
+              onClick={() => choiceRepo({ repositoryName: currentRepo })}
+              className="bg-[#214d35] hover:bg-[#276e48] text-white flex-1"
+              disabled={!!currentRepo}
+            >
+              연동하기
             </Button>
-          </DialogClose>
-        </DialogFooter>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
