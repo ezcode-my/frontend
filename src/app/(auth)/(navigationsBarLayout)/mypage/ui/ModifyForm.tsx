4065558ea38a7e28ab16b53e8c14375fee9a8c02
leetaewon <tw@stayorder.co.kr>
2025-08-25 18:42:17 +0900

    fix : 마이페이지 언어 설정

--- src/app/(auth)/(navigationsBarLayout)/mypage/ui/ModifyForm.tsx
+++ src/app/(auth)/(navigationsBarLayout)/mypage/ui/ModifyForm.tsx
@@ -156,7 +156,7 @@ export const ModifyForm = ({
               entireOption={false}
               option={languageList}
               title="언어선택"
-              value={editForm.language}
+              value={languageList}
               setValue={(value) => {
                 setEditForm((prev) => ({ ...prev, language: Number(value) }));
               }}
