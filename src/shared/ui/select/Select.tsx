f76b1201b35235ca5db2194cfa0693b75c4a2330
leetaewon <tw@stayorder.co.kr>
2025-08-25 18:44:21 +0900

    fix : select 클릭 시 value를 변경하면 사용자가 보는 화면에서 label이 변경되어야하지만 value가 보이는 이슈 수정

--- src/shared/ui/select/Select.tsx
+++ src/shared/ui/select/Select.tsx
@@ -72,7 +72,7 @@ export const Select = ({
     lg: 'h-15 text-base w-100 px-4',
   };
   const sizeClass = sizeMap[size];
-
+  const selectedOption = option.find((opt) => typeof opt.value === 'string' && opt.value === value);
   const handleSelectChange = (value: string | (() => void)) => {
     // type 분기
     if (type === 'setValue') {
@@ -116,7 +116,7 @@ export const Select = ({
         )}
         aria-expanded={dropdown}
       >
-        <span className="truncate">{value || '전체'}</span>
+        <span>{selectedOption?.label || '전체'}</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className={twMerge(
