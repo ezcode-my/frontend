9f292da45e19b7f99a91c996d23a4e39fd2ad04b
leetaewon <tw@stayorder.co.kr>
2025-09-12 09:31:57 +0900

    fix : dialogButton hydration 이슈 해결

--- src/widgets/globalFloatingWidget/ui/index.tsx
+++ src/widgets/globalFloatingWidget/ui/index.tsx
@@ -5,17 +5,23 @@ import GameModalButton from '@/features/game/ui/GameButton';
 import { useCheckCharacterQuery } from '@/entities/game/model/query/game.query';
 import { usePathname } from 'next/navigation';
 import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 export function GlobalFloatingWidget() {
   const problemId = usePathname().split('/')[2];
 
   // const { data: session } = useSession();
-  const isLogged = Cookies.get('accessToken');
 
+  const [isLogged, setIsLogged] = useState(false);
   const { data: characterData } = useCheckCharacterQuery(!!isLogged);
 
   if (problemId) {
     return null;
   }
+  useEffect(() => {
+    if (Cookies.get('accessToken')) {
+      setIsLogged(true);
+    }
+  }, []);
   return (
     <div className="fixed top-[calc(100vh-15%)] left-[calc(100vw-5%)] flex flex-col gap-2">
       {isLogged && <ChatDialogOpenButton />}
