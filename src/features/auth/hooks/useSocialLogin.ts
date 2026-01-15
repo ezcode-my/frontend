56b69e7549a6b5fe8ed2724b83fcc262754c9a0f
JiHoon <gkwlgnd@naver.com>
2026-01-15 18:25:53 +0900

    fix: 소셜로그인 url 수정

--- src/features/auth/hooks/useSocialLogin.ts
+++ src/features/auth/hooks/useSocialLogin.ts
@@ -79,7 +79,7 @@ const useSocialLogin = (onLoginSuccess?: () => void) => {
 
   const handleSocialLogin = async (provider: 'github' | 'google') => {
     try {
-      window.location.href = `https://api.ezcode.my/oauth2/authorize/${provider}?redirect_uri=https://ezcode.my/signin`;
+      window.location.href = `https://api.ezcode.my/api/oauth2/authorize/${provider}?redirect_uri=https://ezcode.my/signin`;
     } catch (error) {
       console.error(error);
     }
